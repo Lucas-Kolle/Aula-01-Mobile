4684ea303eaa02a44e7e65dc217d2590c8749a56
Lucas Kolle <lucaskolle2020@gmail.com>
2026-08-17 08:40:10 -0300

    Enviano dados

---
+++ src/Main.kt
@@ -0,0 +1,26 @@
+// Comentário em linha
+
+/*
+    * Comentário
+    * Em
+    * Blocos
+*/
+
+/*
+    Variáveis em Kotlin:
+
+        var => o valor pode ser alterado
+        var idade = 27
+
+        val => O valor não pode ser alterado
+        val pi = 3.14
+*/
+
+fun main() {
+    println("Hello World!")
+
+    val pi = 3.14
+    var idade = 27
+
+    idade = 30
+}
